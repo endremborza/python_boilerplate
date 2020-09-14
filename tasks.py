d36ced63843ba298f5cc77bb8d1b5b46938ce605
Endre Márk Borza <endremborza@gmail.com>
2020-09-14 22:22:14 +0200

    move stuff out to bran

--- tasks.py
+++ tasks.py
@@ -1,12 +1,8 @@
 from invoke import Collection
 
-from invoke_commands import docs, clean, sonar, test, release, misc
+from branthebuilder import docs, clean, sonar, test, release, misc, django
 
 
 ns = Collection()
-ns.add_collection(Collection.from_module(release))
-ns.add_collection(Collection.from_module(docs))
-ns.add_collection(Collection.from_module(clean))
-ns.add_collection(Collection.from_module(sonar))
-ns.add_collection(Collection.from_module(test))
-ns.add_collection(Collection.from_module(misc))
+for module in [docs, clean, sonar, test, release, misc, django]:
+    ns.add_collection(Collection.from_module(module))
